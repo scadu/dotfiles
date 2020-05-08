3b86aa28069e35a972562a74e871fa74cf1f4ad1
Lukasz Jendrysik <luke@Lukaszs-MacBook-Pro.local>
2020-05-08 19:25:19 +0200

    Update ZSH config and bootstrap script, add Brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,38 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-cd "$(dirname "${BASH_SOURCE}")";
+OH_MY_ZSH_PATH="$HOME/.oh-my-zsh"
 
-git pull origin master;
+if ! command -v 'xcodebuild' >/dev/null 2>&1; 
+then
+	xcode-select --install
+fi
+
+if [[ ! -d $OH_MY_ZSH_PATH/.git ]]
+then 
+	git clone https://github.com/ohmyzsh/ohmyzsh $OH_MY_ZSH_PATH -q
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k -q
+else
+	git -C $OH_MY_ZSH_PATH pull -q
+	git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k pull -q
+fi
+
+brew bundle
 
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
+		--exclude ".macos" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude "Brewfile" \
+		--exclude "Brewfile.lock.json" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read "?This may overwrite existing files in your home directory. Are you sure? (y/n) ";
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
 	fi;
-fi;
 unset doIt;
